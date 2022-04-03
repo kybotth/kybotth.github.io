d151473b2307c1ce28e2752c17c5d28920168f15
James Klein <jamesk@thesabresolution.com>
2022-04-03 00:41:26 -0400

    Deploy Next.js to gh-pages

--- out/_next/static/gGKzvfcpXqxnM2RjQ0jPb/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/573-1fbe78de74e345df.js","static/chunks/pages/index-c4dfaf8c33dff4e3.js"],"/_error":["static/chunks/pages/_error-8022dacb1937fc7a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
